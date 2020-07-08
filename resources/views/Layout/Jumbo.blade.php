befff00814f0d4f115825bc7df7873ff186f4712
Andres E Montbrun <amontbrun@yahoo.com>
2020-07-08 15:35:15 -0400

    2d Commit

--- resources/views/Layout/Jumbo.blade.php
+++ resources/views/Layout/Jumbo.blade.php
@@ -1,8 +1,8 @@
 <div class="container">
     <div class="jumbotron">
-      <h1>Recuperando Conocimientos</h1>      
+      <h1>Recuperando Conocimientos</h1>
       <p>Vamos a ver si de una vez por todas aprendo algo de esta verga!!</p>
     </div>
-    <p>This is some text.</p>      
-    <p>This is another text.</p>      
+    <p>This is some text.</p>
+    {{-- <p>This is another text.</p>       --}}
   </div>
