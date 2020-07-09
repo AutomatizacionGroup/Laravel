c0be072302f13d45bdbaeef6986cd70a7acc1b38
Andres E Montbrun <amontbrun@yahoo.com>
2020-07-09 08:13:14 -0400

    lee de la base de datos

--- routes/web.php
+++ routes/web.php
@@ -13,30 +13,30 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-/*
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('wellcome');
 });
 
-Route::get('/page', function () {
-    return view('page');
-});
+// Route::get('/page', function () {
+//     return view('page');
+// });
 
-Route::get('/about', function () {
-    return "Hi Iam About";
-});
+// Route::get('/about', function () {
+//     return "Hi Iam About";
+// });
 
-Route::get('/contact', function () {
-    return "I am Contact";
-}); */
+// Route::get('/contact', function () {
+//     return "I am Contact";
+// });
 
 // Route::get('/post/{id}', 'PostController@index');
 
-Route::resource('posts','PostController');
+// Route::resource('posts','PostController');
 
-Route::get('/contact', 'PostController@contact');
+// Route::get('/contact', 'PostController@contact');
 
-Route::get('post/{id}/{name}/{paswword}', 'PostController@show_post');
+// Route::get('post/{id}/{name}/{paswword}', 'PostController@show_post');
 
 /* Route::get('admin/posts/example', array('as' => 'admin.home',function(){
 
@@ -46,3 +46,27 @@ Route::get('post/{id}/{name}/{paswword}', 'PostController@show_post');
         return "this url is ". $url;
 
 })); */
+
+// Route::get('/insert', function () {
+
+//     DB::insert('insert into posts (title, content) values (?, ?)', ['este es el primer texto', 'que pondre por el dia de hoy']);
+
+// });
+
+Route::get('/read', function () {
+
+   $results = DB::select('select * from posts where id = ?', [1]);
+
+
+            return var_dump($results);
+
+        // foreach ($results as $post) {
+
+
+
+        //         return $post->title; }
+
+
+
+
+});
